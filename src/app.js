5e0520573fbf73f222caf5723e664d34216a34f4
cclabdis <cclabdis@gmail.com>
2023-04-10 15:39:44 -0300

    TEST

--- src/app.js
+++ src/app.js
@@ -5,21 +5,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const users = []
 
-// const user =
-// {
-// 	username: 'bobesponja', 
-// 	avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png" 
-// }
+function isValidUser(user) {
+  const keys = Object.keys(user);
+  return keys.length === 2 && keys.includes('username') && keys.includes('avatar');
+}
 
+function isValidTweet(tweet) {
+  const keys = Object.keys(tweet);
+  let keysTest = keys.length === 2 && keys.includes('username') && keys.includes('tweet');
 
-// const feed =
-// {
-// 	username: "bobesponja",
-//     tweet: "Eu amo hambúrguer de siri!"
-// }
-
-  const users = []
+  return keysTest && typeof tweet === 'string' && tweet.length > 0 
+}
 
 app.post('/sign-up', (req, res) => {
   const { username, avatar } = req.body;
@@ -34,21 +32,27 @@ app.post('/sign-up', (req, res) => {
 app.post('/tweets', (req, res) => {
   const { username, tweet} = req.body;
   const tweets = {username, tweets};
-  const user = users.find(user => user.username === username );
+  const user = users.find(user => user.username === username);
 
-  if (!user) {
+  if (!isValidUser(user)) {
     res.status(401).send('UNAUTHORIZED');
     return;
   }
 
+  if(!isValidTweet(tweet)) {
+    res.status(400).send('BAD REQUEST');
+    return;
+  }
+
+
   feed.push(tweets);
-  res.json(tweets).status(200).send('OK');
+  res.json(tweets).status(200).send('OK/CREATED');
 });
 
 app.get('/tweets', (req, res) => {
   const { username, avatar, tweet} = req.body;
-  const latest = { username, avatar, tweet }
-  const last10 = latest.length > 10 ? latest.slice(-10): latest ;
+  const latest = { username, avatar, tweet };
+  const last10 = latest.length > 10 ? latest.slice(-10): latest;
 
   res.json(last10);
 });
