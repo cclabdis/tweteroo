a753c8f646b3a982aa825b9c71591e5da48b97b2
cclabdis <cclabdis@gmail.com>
2023-06-26 16:48:04 -0300

    add endpoint post;tweets for hub

--- src/app.js
+++ src/app.js
@@ -8,69 +8,48 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
-
 const users = []
 
-
-function isValidTweet(tweet) {
-  const keys = Object.keys(tweet);
-  let keysTest = keys.length === 2 && keys.includes('username') && keys.includes('tweet');
-
-  return keysTest && typeof tweet === 'string' && tweet.length > 0 
-}
+const feed = []
 
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
+
+  if (!username || !avatar) {
+    return res.status(400).send("Todos os campos são obrigatórios!")
+}
   const user = ({ username, avatar });
   users.push(user);
   res.send("ok");
 })
 
-//n esta me retornando ok
 
-const feed = [];
+app.post("/tweets", (req, res) => {
 
-app.post('/tweets', (req, res) => {
-  const { username, tweet} = req.body;
-  const tweets = {username, tweets};
-  const user = users.find(user => user.username === username);
+    const tweet = req.body.tweet
+    const user = req.headers.username
+
+    if (!user || !tweet) {
+        return res.status(400).send("Todos os campos são obrigatórios!")
+    }
+
+    const userExists = users.find(data => data.username === user)
+
+    if (!userExists) {
+        return res.status(401).send("UNAUTHORIZED")
+    }
+
+    feed.unshift({ tweet, user })
+
+    if (feed.length > 10) {
+        feed.pop()
+    }
+
+    res.status(201).send("OK")
+
+})
 
 
-  function isValidUser(user) {
-    const keys = Object.keys(user);
-    return keys.length === 2 && keys.includes('username') && keys.includes('avatar');
-  }
-  
-  if (!user && !isValidUser(user)) return res.status(401).send('UNAUTHORIZED')
-
-  
-    
-  feed.push(tweets)
-  res.json(tweets).status(200).send('OK/CREATED');
-
-  if(!isValidTweet(tweet)) {
-    res.status(400).send('BAD REQUEST');
-    return;
-  }
-
-  
-});
-
-app.get('/tweets', (req, res) => {
-  const { username, avatar, tweet} = req.body;
-  const latest = { username, avatar, tweet };
-  const last10 = latest.length >= 10 ? latest.slice(-10): latest;
-  latest.length === 0;
-
-  if(latest.length === 0) {
-    res.json(feed);
-    return;
-  }
-
-  res.json(last10);
-});
 
 
 const PORT = 5000;
